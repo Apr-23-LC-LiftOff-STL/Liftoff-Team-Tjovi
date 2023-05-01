8b00ab69edbd4f3e656632322525e8ec6ea9d49a
Geoffrey Meier <grm0809@gmail.com>
2023-05-01 16:28:17 -0500

    unfinished, trying to resolve JDBC error

--- src/main/java/com/liftoff/ecommerce/Repositories/ShoppingCartRepository.java
+++ src/main/java/com/liftoff/ecommerce/Repositories/ShoppingCartRepository.java
@@ -3,6 +3,7 @@ package com.liftoff.ecommerce.Repositories;
 import com.liftoff.ecommerce.Models.ShoppingCart;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 
 import java.util.List;
