623107e985ff605e3b9e9b28ddf044294c2756d7
Grant Crawford <grantttt.crawford@gmail.com>
2023-05-04 00:37:01 -0500

    Added sort by mulitple genres.

--- src/main/java/com/liftoff/ecommerce/Specifications/MovieSpecification.java
+++ src/main/java/com/liftoff/ecommerce/Specifications/MovieSpecification.java
@@ -2,13 +2,28 @@ package com.liftoff.ecommerce.Specifications;
 
 import com.liftoff.ecommerce.Models.Movie;
 import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MovieSpecification {
 
     //TODO: Add multiple genre option -- look into exclusivity?
-    public static Specification<Movie> hasGenre(String genreName) {
-        return  StringUtils.isBlank(genreName) ? null : (movie, cq, cb) -> cb.equal(movie.join("genres").get("name"), genreName);
+    public static Specification<Movie> hasGenre(String genreNames) {
+        return (StringUtils.isBlank(genreNames))
+                ? null
+                : (movie, cq, cb) -> {
+            List<String> genreList = Arrays.asList(genreNames.split(","));
+            Predicate[] genrePredicates = new Predicate[genreList.size()];
+            int index = 0;
+            for (String genreName : genreList) {
+                genrePredicates[index] = cb.equal(movie.join("genres").get("name"), genreName);
+                index++;
+            }
+            return cb.and(genrePredicates);
+        };
     }
 
     public static Specification<Movie> hasTitle(String title) {
