bf826a5eadd7116402da7e1daf7b4d6fa04b33a4
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-05-04 12:06:38 -0500

    MovieCard css updated for animation, drop shadow

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -4,6 +4,18 @@ import "./MovieCards.css"
 const MovieCard = ({ title, posterPath }) => {
   const baseImgUrl = 'https://image.tmdb.org/t/p/w500';
 
+  const expression = /\s[^\s]*$/;
+
+  const createShortcut = (text, limit) => {
+      if (text.length > limit) {
+          const part = text.slice(0, limit - 3);
+          if (part.match(expression)) {
+              return part.replace(expression, '...');
+          }
+          return part + '...';
+      }
+      return text;
+  };
 
   return (
     <div className="movie-card">
@@ -12,8 +24,8 @@ const MovieCard = ({ title, posterPath }) => {
           <img src={`${baseImgUrl}${posterPath}`} alt={`Poster for ${title}`} />
         )}
       </div>
-      <div className="">
-        <div className="">{title}</div>
+      <div className="movie-card-black">
+        <div className="">{createShortcut(title, 40)}</div>
       </div>
     </div>
   );
