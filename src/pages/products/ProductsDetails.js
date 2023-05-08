e334af567f4fd12c696647d7aaf8cd4fa80c8f74
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-05-07 22:55:52 -0500

    f1400 ProductDetails.js revised styling, add-to-cart button on screen message added

--- src/pages/products/ProductsDetails.js
+++ src/pages/products/ProductsDetails.js
@@ -1,6 +1,9 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useState } from 'react';
 import "./Products.css";
+
+// https://responsive-bulma-cards.netlify.app/ - example #5
 
 export default function ProductsDetails() {
   const { id } = useParams();
@@ -8,14 +11,21 @@ export default function ProductsDetails() {
 
   const baseImgUrl = "https://image.tmdb.org/t/p/w400";
 
+  const [addToCart, setAddToCart] = useState("");
+  const [addToCartButtonStyle, setAddToCartButtonStyle] = useState("is-primary");
+  const addToCartButtonHandler = (e) => {
+    setAddToCart(`"${product.title}" was added to cart!`);
+    setAddToCartButtonStyle("is-success");
+  };
+
   return (
     <div>
       <section className="section">
         <div className="container">
-          <div className="columns">
+          <div className="columns is-centered">
             <div className="column">
-              <div className="card is-horizontal shadow-md transform is-duration-300 hover-shadow-xl hover-translate-y">
-                <div className="card-image">
+              <div className="card is-horizontal shadow-xl transform is-duration-100">
+                <div className="card-image p-4">
                   <figure className="image">
                     <img
                       src={`${baseImgUrl}${product.posterPath}`}
@@ -23,8 +33,8 @@ export default function ProductsDetails() {
                     ></img>
                   </figure>
                 </div>
-                <div className="card-content p-0 is-flex is-flex-direction-column">
-                  <div className="content p-5 has-text-weight-normal">
+                <div className="card-content p-4 is-flex is-flex-direction-column">
+                  <div className="content p-4 has-text-weight-normal">
                     <div className="is-size-5">
                       <h3>{product.title}</h3>
                     </div>
@@ -32,12 +42,12 @@ export default function ProductsDetails() {
                       {product.overview}
                     </p>
                     <p>
-                      <span className="has-text-weight-semibold">Genres: </span>
+                      <span className="has-text-weight-semibold">&emsp; &emsp; Genres: </span>
                       {product.genres}
                     </p>
                     <p>
                       <span className="has-text-weight-semibold">
-                        Runtime:{" "}
+                      &emsp; &emsp; Runtime:{" "}
                       </span>
                       {product.runtime} minutes
                     </p>
@@ -45,30 +55,26 @@ export default function ProductsDetails() {
                   <div className="content p-5 has-background-info-light">
                     <div className="columns">
                       <div className="column">
-                        <div className="is-size-5">
+                        <div className="is-size-4">
                           <span className="has-text-weight-semibold">
                             Price:
                           </span>{" "}
                           ${product.price.toFixed(2)}
+                          <span className="is-italic is-size-6 has-text-danger pl-5">{addToCart}</span>
                         </div>
-                        <div className="is-size-6">
-                          <span className="has-text-primary-dark">
-                            <br />
-                            &#9733;&#9733;&#9733;&#9733;&#9733;
-                          </span>{" "}
-                          10 Reviews
-                        </div>
-                      </div>
-                      <div className="column">
-                        <button className="button is-primary is-normal is-fullwidth has-text-weight-semibold">
+                        <div>
+                        
+                        <br />
+                        <button className="button is-medium is-primary is-fullwidth has-text-weight-semibold" onClick={addToCartButtonHandler}>
                           Add to Cart
                         </button>
                         <br />
                         <NavLink to="/">
-                          <button className="button is-normal is-fullwidth">
+                          <button className="button is-normal is-link is-fullwidth">
                             Home
                           </button>
                         </NavLink>
+                      </div>
                       </div>
                     </div>
                     <p className="is-size-7"></p>
