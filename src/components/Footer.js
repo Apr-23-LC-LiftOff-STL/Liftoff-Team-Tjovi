a6a0e74088279cb6dbdb678abe8a42d78f34b9ba
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-05-07 20:54:07 -0500

    f1100 footer restyled, logo added, footer links restyled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,41 @@
 import { Outlet, NavLink } from "react-router-dom";
 import "bulma/css/bulma.css";
+import logo125 from "./Logo_MovieDL_20230426_125x22.png";
 
 const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="content has-text-centered"></div>
-      <div class="columns">
-        <div class="column"></div>
-        <div class="column"></div>
-        <div class="column">
-          <strong>MovieDL</strong>
-          <p>4811 Delmar Blvd, St. Louis, MO 63108</p>
-          <p>(314) 254-0107</p>
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <NavLink to="/">
+          <img src={logo125} />
+        </NavLink>
+        <p className="is-size-7">
+          4811 Delmar Blvd
+          <br />
+          St. Louis, MO 63108
+          <br />
+          (314) 254-0107
+        </p>
+        <div class="tabs is-centered">
+          <ul>
+            <li>
+              <NavLink className="card-footer-item" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="card-footer-item" to="help">
+                Customer Service
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="card-footer-item" to="about">
+                About
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <div class="column">
-          <br></br>
-          <div>
-            <NavLink to="help">Customer Service</NavLink>
-          </div>
-          <div>
-            <NavLink to="about">About</NavLink>
-          </div>
-        </div>
+        <div className="content has text-centered"></div>
       </div>
     </footer>
   );
