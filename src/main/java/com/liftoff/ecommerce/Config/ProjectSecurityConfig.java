9eae76929bf30ca9d13f788639658ef22b1beb09
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-05-08 13:07:19 -0500

    ProjectSecurityConfig class' defaultSecurityFilterChain method includes CSRF and CORs code to allow for connection to front end. Also, added new filter package with CsrfCookieFilter class necessary for CSRF code in defaultSecurityFilterChain method. Have successfully tested functionality in Postman.

--- src/main/java/com/liftoff/ecommerce/Config/ProjectSecurityConfig.java
+++ src/main/java/com/liftoff/ecommerce/Config/ProjectSecurityConfig.java
@@ -1,25 +1,52 @@
 package com.liftoff.ecommerce.Config;
 
 
+import com.liftoff.ecommerce.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable()
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext().requireExplicitSave(false)
+            .and().sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+            .cors().configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+               CorsConfiguration config=new CorsConfiguration();
+               config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+               config.setAllowedMethods(Collections.singletonList("*"));
+               config.setAllowCredentials(true);
+               config.setAllowedHeaders(Collections.singletonList("*"));
+               config.setMaxAge(3L);
+               return config;
+            }
+        }).and().csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
-                .requestMatchers("/movies/{id}").authenticated()
-                .requestMatchers("/movies", "/login", "/register").permitAll()
+                    .requestMatchers("/movies/{id}").authenticated()
+                    .requestMatchers("/movies", "/login", "/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
-
         return http.build();
     }
 
--- src/main/java/com/liftoff/ecommerce/Controllers/CustomerController.java
+++ src/main/java/com/liftoff/ecommerce/Controllers/CustomerController.java
@@ -6,10 +6,14 @@ import com.liftoff.ecommerce.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -39,5 +43,16 @@ public class CustomerController {
                     .body("An exception occurred due to " + ex.getMessage());
         }
         return response;
+    }
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication){
+        List<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if(customers.size()>0){
+            return customers.get(0);
+        } else{
+            return null;
+        }
+
     }
 }
