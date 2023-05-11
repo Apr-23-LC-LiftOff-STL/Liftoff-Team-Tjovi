eba13e6825d1ac5c1405b3f2a68b49f37c0b5cc2
Tevrese Young <tevrese12@gmail.comgit config --global user.email tevrese12@gmail.comgit config --global user.email tevrese12@gmail.com>
2023-05-10 21:10:30 -0500

    fixed navigate

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 export default function Login() {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     email: "",
     password: "",
