89298677c97ded8cd25e858350b885c32e3b1876
Grant Crawford <grantttt.crawford@gmail.com>
2023-05-11 20:09:09 -0500

    Changed from apiattempt to ecommerce

--- src/main/java/com/liftoff/ecommerce/Controllers/MovieController.java
+++ src/main/java/com/liftoff/ecommerce/Controllers/MovieController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 import static java.lang.Thread.sleep;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000/products/**")
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
