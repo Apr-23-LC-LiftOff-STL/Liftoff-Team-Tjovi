ee0238da939f3ab1b9cce4f89a581c713a039a13
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-05-16 06:36:34 -0500

    added custom authentication provider : UsernamePasswordAuthenticationProvider

--- src/main/java/com/liftoff/ecommerce/Controllers/CustomerController.java
+++ src/main/java/com/liftoff/ecommerce/Controllers/CustomerController.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CustomerController {
 
--- src/main/java/com/liftoff/ecommerce/Controllers/MovieController.java
+++ src/main/java/com/liftoff/ecommerce/Controllers/MovieController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 import static java.lang.Thread.sleep;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
