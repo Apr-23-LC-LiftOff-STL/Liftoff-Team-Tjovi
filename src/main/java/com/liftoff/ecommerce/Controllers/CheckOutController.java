e4bd1c031e7134b03851ddd8ae6a7818e9f6bb60
Geoffrey Meier <grm0809@gmail.com>
2023-05-17 13:58:12 -0500

    added backend for Stripe to checkout

---
+++ src/main/java/com/liftoff/ecommerce/Controllers/CheckOutController.java
@@ -0,0 +1,28 @@
+package com.liftoff.ecommerce.Controllers;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class CheckOutController {
+
+    @PostMapping("/checkout")
+    public Map<String,String> checkout() throws StripeException {
+        Stripe.apiKey="sk_test_51N8n2ODvHmrdraF8ov56fzzBxwokVlEvCMG8tHuBBZZCdWZT39hK9QYonV7aHiT0UwOUgsBgWTJOe97UgHBwxEoH000oqXg4Qu";
+
+        PaymentIntentCreateParams params= PaymentIntentCreateParams.builder().setAmount(100L).setCurrency("usd").setAutomaticPaymentMethods(PaymentIntentCreateParams.AutomaticPaymentMethods.builder().setEnabled(true).build()).build();
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+        Map<String,String> map = new HashMap<>();
+        map.put("client_secret",paymentIntent.getClientSecret());
+        return map;
+    }
+
+}
--- src/main/java/com/liftoff/ecommerce/ECommerceApp.java
+++ src/main/java/com/liftoff/ecommerce/ECommerceApp.java
@@ -1,13 +1,15 @@
 package com.liftoff.ecommerce;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ECommerceApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(ECommerceApp.class, args);
+
     }
 
 }
