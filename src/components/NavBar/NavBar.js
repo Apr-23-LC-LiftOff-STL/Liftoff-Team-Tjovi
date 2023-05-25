87476d9dfb3fee584397545d192330ec5c337bf0
Tevrese Young <tevrese12@gmail.comgit config --global user.email tevrese12@gmail.comgit config --global user.email tevrese12@gmail.com>
2023-05-24 21:46:23 -0500

    current iteration of NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -25,15 +25,8 @@ const NavBar = () => {
   const [isActive, setisActive] = useState(false);
   const [cartButtonStyling, setCartButtonStyling] = useState();
   const [cartDropdownStyling, setCartDropdownStyling] = useState();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setIsLoggedIn(true);
-    } 
-  }, [isLoggedIn]);
-  
-   useEffect(() => {
     if (totalProductsInCart !== 0) {
       setCartButtonStyling(
         "button is-warning has-text-weight-semibold is-hidden-mobile"
@@ -44,11 +37,7 @@ const NavBar = () => {
       setCartDropdownStyling("has-text-weight-normal");
     }
   }, [totalProductsInCart]);
-  
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setIsLoggedIn(false);
-  };
+
   return (
     <nav
       className="navbar pt-1 pl-4 pr-4"
@@ -79,7 +68,6 @@ const NavBar = () => {
       <div className="navbar-item">
         <SearchBar />
       </div>
-
       <div className="navbar-end">
         <div
           id="navbarBasicExample"
@@ -121,19 +109,12 @@ const NavBar = () => {
             </div>
           </div>
 
-          <div className="buttons">
-            <a
-              className="button is-primary is-hidden-mobile"
-              href="/login"
-            >
-              Log in
-            </a>
-          )}
+
 
           <div className="buttons">
-            {/* <a className="button is-primary is-hidden-mobile" href="/login">
+            <a className="button is-primary is-hidden-mobile" href="/login" >
               Log in
-            </a> */}
+            </a>
             <a
               className={cartButtonStyling}
               style={{ width: "92px" }}
