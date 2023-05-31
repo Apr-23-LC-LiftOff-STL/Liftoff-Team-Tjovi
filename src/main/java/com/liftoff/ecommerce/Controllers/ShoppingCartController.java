6f8b6b69ce5f57fc356e40fc4e769088e9191ff3
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-05-31 13:30:19 -0500

    minor changes

--- src/main/java/com/liftoff/ecommerce/Controllers/ShoppingCartController.java
+++ src/main/java/com/liftoff/ecommerce/Controllers/ShoppingCartController.java
@@ -2,8 +2,6 @@ package com.liftoff.ecommerce.Controllers;
 
 import com.liftoff.ecommerce.Models.Customer;
 import com.liftoff.ecommerce.Models.ShoppingCart;
-import com.liftoff.ecommerce.Repositories.CustomerRepository;
-import com.liftoff.ecommerce.Repositories.MovieRepository;
 import com.liftoff.ecommerce.Repositories.ShoppingCartRepository;
 import com.liftoff.ecommerce.Service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,38 +51,3 @@ public class ShoppingCartController {
     }
 }
 
-//    @GetMapping("/remove/{id}/{movieId}")
-//    public void removeFromCart(@PathVariable Long id,@PathVariable Long movieId){
-//        shoppingCartService.removeFromCart(id, movieId);
-//    }
-
-//    @GetMapping("/cart/{id}")
-//    public Optional<ShoppingCart> returnCart(@PathVariable Long id) throws Exception{
-//        return shoppingCartRepository.findById(id);
-//    }
-
-
-
-// Geoff's original code
-//
-//    @GetMapping("/createCart/{email}")
-//    public void createCartForUser(@PathVariable String email){
-//        shoppingCartService.createShoppingCartClass(email);
-//    }
-//    @GetMapping("/createCart/{customerId}")
-//    public void createCartForUser(@PathVariable Integer customerId){
-//        shoppingCartService.createShoppingCartClass(customerId);
-//    }
-//    @GetMapping("/add/{id}/{movieId}")
-//    public void addToCart(@PathVariable int id,@PathVariable Long movieId){
-//        shoppingCartService.addToCart(movieId,id);
-//    }
-//    @GetMapping("/remove/{id}/{movieId}")
-//    public void removeFromCart(@PathVariable int id,@PathVariable Long movieId){
-//        shoppingCartService.removeFromCart(movieId,id);
-//    }
-//
-//    @GetMapping("/cart/{id}")
-//    public Optional<ShoppingCart> seeCart(@PathVariable int id) throws Exception{
-//        return shoppingCartRepository.findById(id);
-//    }
--- src/main/java/com/liftoff/ecommerce/Service/ShoppingCartService.java
+++ src/main/java/com/liftoff/ecommerce/Service/ShoppingCartService.java
@@ -12,13 +12,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 
 @Service
 public class ShoppingCartService {
+
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
@@ -95,122 +94,5 @@ public class ShoppingCartService {
 
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
         shoppingCart.setTotalPrice(Double.parseDouble(decimalFormat.format(totalPrice)));
-
     }
 }
-//            ShoppingCart shoppingCart = new ShoppingCart(customer.get(0),new ArrayList<>());
-//            shoppingCart.setMovieIds(new ArrayList<>());
-//            shoppingCartRepository.save(shoppingCart);
-
-
-// Geoff's original code
-//    public void createShoppingCartClass(Long customerId){
-//        if (customerRepository.existsById(customerId)){
-//            ShoppingCart shoppingCart = new ShoppingCart(customerId,new ArrayList<>());
-//            shoppingCart.setMovieIds(new ArrayList<>());
-//            shoppingCart.setId(customerId);
-//            shoppingCartRepository.save(shoppingCart);
-//        }
-
-
-//    public ArrayList<Long> createCart(int userId) {
-//
-//        if (shoppingCartRepository.existsById(userId)){
-//            movies = new ArrayList<>();
-//            return movies;
-//
-//    }
-//        return null;
-//    }
-
-//    public void addToCart(Long id, Long movieId){
-//
-//        if (movieRepository.existsById(movieId) && shoppingCartRepository.existsById(id)){
-//
-//            shoppingCartRepository.findById(id).get().addToCart(movieId);
-//            shoppingCartRepository.save(shoppingCartRepository.findById(id).get());
-//
-//        }
-//    }
-//    public void removeFromCart(Long id, Long movieId){
-//        if (movieRepository.existsById(movieId) && shoppingCartRepository.existsById(id)){
-//            shoppingCartRepository.findById(id).get().deleteFromCart(movieId);
-//            shoppingCartRepository.save(shoppingCartRepository.findById(id).get());
-//        }
-//    }
-//    public void addToCart(String email, ShoppingCart shoppingCart){
-//        Long movieId = shoppingCart.getMovieId();
-//        Long quantity = shoppingCart.getQuantity();
-//        // if we add customerId to JWT payload, FE could just pass customerId in the
-//        // request body with the other shoppingCart fields, removing this findByEmail step
-//        Customer customer = findCustomer(email);
-//        Long customerId = customer.getId();
-//
-//        List<ShoppingCart> customerCart = shoppingCartRepository.findByCustomerId(customerId);
-//        // If Customer(id) has no carts
-//        if(customerCart.size()==0){
-//            createNewShoppingCart(customer, movieId, quantity);
-//        } else if(){
-//
-//
-//            for (ShoppingCart currentCart:customerCart) {
-//                // If movieId wanting to be added to cart already exists in cart
-//                if(currentCart.getMovieId().equals(movieId)){
-//                    updateQuantityInCart(currentCart, quantity);
-//                    break;
-////                if(currentCart.getMovieId()==(movieId)){
-////                    updateQuantityInCart(currentCart, quantity);
-//
-//                    // If movieId wanting to be added to cart does not already exist in cart
-//                }
-//            }
-//        }
-//    }
-
-//    public void addToCart(String email, ShoppingCart shoppingCart){
-//        Customer customer = findCustomer(email);
-//        Long customerId = customer.getId();
-//        Long movieId = shoppingCart.getMovieId();
-//        Long quantity = shoppingCart.getQuantity();
-//        Long duplicateCartId = customerHasCartItemMatchingNewMovieId(customerId, movieId);
-//
-//
-//        //if customer already has a cart item matching movieId
-//        if(duplicateCartId!=null){
-//            Optional<ShoppingCart> duplicateCart = shoppingCartRepository.findById(duplicateCartId);
-//            mergeDuplicateCarts(duplicateCart.get(), shoppingCart);
-//        // if customer does not have a cart item matching movieId
-//        }
-//        if(duplicateCartId==null){
-//            createNewShoppingCart(customer, movieId, quantity);
-//        }
-//    }
-
-//    public Long customerHasCartItemMatchingNewMovieId(Long customerId, Long movieId){
-//        Long duplicateCartId=null;
-//        List<ShoppingCart> customerCart = shoppingCartRepository.findByCustomerId(customerId);
-//        if(customerCart.size()>0){
-//            for(ShoppingCart currentCart:customerCart){
-//                if(currentCart.getMovieId().equals(movieId)){
-//                    duplicateCartId = currentCart.getCartId();
-//                }
-//            }
-//        }
-//        return duplicateCartId;
-//    }
-
-
-
-//    public void mergeDuplicateCarts(ShoppingCart oldCart, ShoppingCart newCart){
-//        Long mergedQuantity = oldCart.getQuantity() + newCart.getQuantity();
-//        Customer mergedCustomer = oldCart.getCustomer();
-//        Long mergedMovieId = oldCart.getMovieId();
-//        ShoppingCart mergedCart = new ShoppingCart();
-//        mergedCart.setCustomer(mergedCustomer);
-//        mergedCart.setMovieId(mergedMovieId);
-//        mergedCart.setQuantity(mergedQuantity);
-//        setTotalPrice(mergedCart);
-//        shoppingCartRepository.save(mergedCart);
-//        deleteOldDuplicateCart(oldCart.getCartId());
-//    }
-
