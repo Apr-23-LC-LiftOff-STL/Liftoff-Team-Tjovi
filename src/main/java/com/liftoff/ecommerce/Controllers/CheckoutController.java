a162d19b128985c5aca4df4b81c6343f40fc6eeb
Geoffrey Meier <grm0809@gmail.com>
2023-06-01 12:30:34 -0500

    added stripe checkout

--- src/main/java/com/liftoff/ecommerce/Controllers/CheckoutController.java
+++ src/main/java/com/liftoff/ecommerce/Controllers/CheckoutController.java
@@ -4,6 +4,7 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -12,11 +13,15 @@ import java.util.Map;
 @RestController
 public class CheckoutController {
 
+    private String key;
 
     @RequestMapping("/checkout")
-    public Map<String,String> checkout() throws StripeException {
-        Stripe.apiKey="sk_test_51N8n2ODvHmrdraF8ov56fzzBxwokVlEvCMG8tHuBBZZCdWZT39hK9QYonV7aHiT0UwOUgsBgWTJOe97UgHBwxEoH000oqXg4Qu";
-   PaymentIntentCreateParams params= PaymentIntentCreateParams.builder().setAmount(200l).setCurrency("usd").setAutomaticPaymentMethods(PaymentIntentCreateParams.AutomaticPaymentMethods.builder().setEnabled(true).build()).build();
+    public Map<String,String> checkout(@RequestParam Long amount) throws StripeException {
+        Map<String, String> env = System.getenv();
+
+
+        Stripe.apiKey = "sk_test_51N8n2ODvHmrdraF8ov56fzzBxwokVlEvCMG8tHuBBZZCdWZT39hK9QYonV7aHiT0UwOUgsBgWTJOe97UgHBwxEoH000oqXg4Qu";
+   PaymentIntentCreateParams params= PaymentIntentCreateParams.builder().setAmount(amount).setCurrency("usd").setAutomaticPaymentMethods(PaymentIntentCreateParams.AutomaticPaymentMethods.builder().setEnabled(true).build()).build();
 
         PaymentIntent paymentIntent = PaymentIntent.create(params);
         Map<String,String> map = new HashMap<>();
