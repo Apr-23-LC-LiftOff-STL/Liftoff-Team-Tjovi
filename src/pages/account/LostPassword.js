c1d2d1d183f53dbedc37487d38e31b87fe31231a
Tevrese Young <tevrese12@gmail.comgit config --global user.email tevrese12@gmail.comgit config --global user.email tevrese12@gmail.com>
2023-06-07 19:44:20 -0500

    Password changes in database and sends email (unable to login?)

--- src/pages/account/LostPassword.js
+++ src/pages/account/LostPassword.js
@@ -1,87 +1,82 @@
-
 import React, { useState, useRef } from "react";
 import emailjs from "emailjs-com";
+import axios from "axios";
 
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 export default function LostPassword() {
   const [values, setValues] = useState({
     name: "",
     email: "",
-    
   });
 
   const handleChange = (e) => {
     var value = e.target.value === "" ? null : e.target.value;
-
     setValues({
       ...values,
       [e.target.name]: value,
     });
   };
 
-  const handleSubmit = (e) => { 
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newPassword = generateRandomPassword();
-    console.log("These are the handleSubmit values")
-    console.log(values.name)
-    console.log(values.email)
-    console.log(newPassword)
-  sendEmail( values.name, values.email, newPassword) 
+    console.log("These are the handleSubmit values");
+    console.log(values.name);
+    console.log(values.email);
+    console.log(newPassword);
+    const username = values.email;
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/profile/isUser/${username}`
+        
+      );
 
-
-    // console.log(newPassword)
-    // console.log(newPassword)
-   // setValues({pwd: newPassword })
-    // console.log(values.email)
-     //console.log(values.pwd)
-    // const templateParams = {
-    //   to_email: values.email,
-    // pwd: values.pwd,
-    //   to_name: values.name,
-    // };
-    // emailjs
-    //   .sendForm(
-    //     "service_i7rn969",
-    //     "template_2v6984n",
-    //     form.current,
-    //     "Cy7tiuWbCkBC_n4MB", 
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //       alert("Your email was successfully sent");
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
+      if (response.status === 200 || response.status === 201) {
+        //toast.success("This user exists");
+        const response = await fetch(`http://localhost:8080/profile/changePassword/${username}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPassword),
+    });
+        
+        sendEmail(values.name, values.email, newPassword);
+        
+        toast.success("Your password was successfully updated!");
+      } else {
+        throw new Error(`Request failed: ${response.status}`);
+      }
+    } catch (error) {
+      
+      toast.error("Password update unsuccessful!");
+    }
   };
-  
 
   const generateRandomPassword = () => {
     const charset =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+";
-
     let newPassword = "";
-    let length = 10
+    let length = 10;
     for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * charset.length);
       newPassword += charset[randomIndex];
     }
-
     return newPassword;
   };
 
-  const sendEmail = (name, email, newPassword) =>{
-    const serviceId = "service_i7rn969"
-    const templateId ="template_2v6984n"
-    const userId = "Cy7tiuWbCkBC_n4MB"
+  const sendEmail = (name, email, newPassword) => {
+    const serviceId = "service_i7rn969";
+    const templateId = "template_2v6984n";
+    const userId = "Cy7tiuWbCkBC_n4MB";
     const templateParams = {
       to_email: email,
       pwd: newPassword,
-      to_name: name
-    }
-emailjs.send(serviceId, templateId, templateParams, userId)
-.then(
+      to_name: name,
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, userId).then(
       (response) => {
         console.log(response.text);
         alert("Your email was successfully sent");
@@ -110,35 +105,10 @@ emailjs.send(serviceId, templateId, templateParams, userId)
         onChange={handleChange}
         required
       />
-  {/* <label>Password</label>
-      <input
-        type="radio"
-        name="pwd"
-        value={values.pwd}
-        onChange={handleChange}
-        required
-      /> */}
-
-      <button type="submit" value="Send" >Send Password</button>
+      <button type="submit" value="Send">
+        Send Password
+      </button>
+      <ToastContainer/>
     </form>
-    // <div form ref={form} onSubmit={sendEmail}>
-    //   <div class="field">
-    //     <label class="label">Email</label>
-    //     <input
-    //       className="input "
-    //       type="email"
-    //       value={values.email}
-    //       onChange={handleChange}
-    //       required
-    //       placeholder="e.g. alexsmith@gmail.com"
-    //       name="email"
-    //     />
-    //   </div>
-    //   <div className="field">
-    //     <p className="control">
-    //     <input type="submit" value="Send" />
-    //     </p>
-    //   </div>
-    // </div>
   );
 }
